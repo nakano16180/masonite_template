2daa14cee376f0052c6de9c1a08c2bf9a5499080
nakano16180 <nakano16180@gmail.com>
2020-02-23 18:33:31 +0900

    [fix] virtualenvは管理対象外に

--- .venv/lib/python3.6/site-packages/masonite/logging/drivers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .LogSingleDriver import LogSingleDriver
-from .LogTerminalDriver import LogTerminalDriver
-from .LogSlackDriver import LogSlackDriver
-from .LogSyslogDriver import LogSyslogDriver
-from .BaseDriver import BaseDriver
